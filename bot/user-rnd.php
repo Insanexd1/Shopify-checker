9cc53ec7130a80c63d7e82f8fd749634d7427db4
Insanexd1 <154810771+Insanexd1@users.noreply.github.com>
2024-03-24 11:57:54 +0000

    Delete bot directory

--- bot/user-rnd.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$uRnd = json_decode(file_get_contents('https://randomuser.me/api/'), 1);
-
-$name = $uRnd['results']['0']['name'];
-$location = $uRnd['results']['0']['location'];
-$mail = str_replace('@example.com', '@gmail.com', $uRnd['results']['0']['email']);
-$login = $uRnd['results']['0']['login'];
